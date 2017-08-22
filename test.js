70b2d30ec64fcb98f00ae13393c764e4072d5612
fernandocode <lealfernandoleal@gmail.com>
2017-08-22 10:51:35 -0300

    settings typescrip

---
+++ test.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+var ExpressionUsage = (function () {
+    function ExpressionUsage() {
+        this._expressionUtils = new index_1.ExpressionUtils();
+    }
+    ExpressionUsage.prototype.expression = function (exp) {
+        console.log(this._expressionUtils.getColumnByExpression(exp));
+    };
+    ExpressionUsage.prototype.test = function () {
+        this.expression(function (x) { return x.id; }); // id
+        this.expression(function (x) { return x.description; }); // description
+        this.expression(function (x) { return x.date; }); // date
+        this.expression(function (x) { return x.isValid; }); // isValid
+        this.expression(function (x) { return x.reference.name; }); // reference_name
+    };
+    return ExpressionUsage;
+}());
+exports.ExpressionUsage = ExpressionUsage;
+var ModelTest = (function () {
+    function ModelTest() {
+    }
+    return ModelTest;
+}());
+var ReferencesModelTest = (function () {
+    function ReferencesModelTest() {
+    }
+    return ReferencesModelTest;
+}());
